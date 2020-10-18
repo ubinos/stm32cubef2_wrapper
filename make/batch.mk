3d08bbb44adc68922b584b700b8465c333f930f3
Sungho Park <siminsungho@gmail.com>
2020-10-18 19:34:20 +0900

    add ubinos config, adc_dualmodeinterleaved_stm3221geval_baremetal, adc_regularconversion_dma_stm3221geval_baremetal, adc_regularconversion_interrupt_nucleof207zg_baremetal, adc_regularconversion_interrupt_stm3221geval_baremetal, adc_regularconversion_polling_stm3221geval_baremetal

--- make/batch.mk
+++ make/batch.mk
@@ -11,28 +11,44 @@
 	make -f makefile.mk $@ CONFIG_DIR=../config CONFIG_NAME=stm32cubef2_stm3221geval_baremetal
 	@echo ""
 	@echo ""
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../config CONFIG_NAME=stm32cubef2_nucleof207zg_baremetal
-	@echo ""
-	@echo ""
-	
 	
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_NAME=myuart_printf_stm3221geval
+	make -f makefile.mk $@ CONFIG_NAME=adc_dualmodeinterleaved_stm3221geval_baremetal
 	@echo ""
 	@echo ""
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_NAME=myuart_printf_nucleof207zg
+	make -f makefile.mk $@ CONFIG_NAME=adc_regularconversion_dma_stm3221geval_baremetal
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_NAME=adc_regularconversion_interrupt_stm3221geval_baremetal
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_NAME=adc_regularconversion_polling_stm3221geval_baremetal
 	@echo ""
 	@echo ""
 	
-	
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_NAME=myadc_dualmodeinterleaved_stm3221geval
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_NAME=myadc_regularconversion_dma_stm3221geval
+	@echo ""
+	@echo ""
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
@@ -45,13 +61,6 @@
 	make -f makefile.mk $@ CONFIG_NAME=myadc_regularconversion_polling_stm3221geval
 	@echo ""
 	@echo ""
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
-	make -f makefile.mk $@ CONFIG_NAME=myadc_regularconversion_dma_stm3221geval
-	@echo ""
-	@echo ""
-	
 	
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
@@ -62,14 +71,36 @@
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_NAME=mygpio_exti_nucleof207zg
+	make -f makefile.mk $@ CONFIG_NAME=mygpio_iotoggle_stm3221geval
 	@echo ""
 	@echo ""
 	
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_NAME=mygpio_iotoggle_stm3221geval
+	make -f makefile.mk $@ CONFIG_NAME=myuart_printf_stm3221geval
+	@echo ""
+	@echo ""
+	
+	
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../config CONFIG_NAME=stm32cubef2_nucleof207zg_baremetal
+	@echo ""
+	@echo ""
+	
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_NAME=adc_regularconversion_interrupt_nucleof207zg_baremetal
+	@echo ""
+	@echo ""
+	
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_NAME=mygpio_exti_nucleof207zg
 	@echo ""
 	@echo ""
 	
@@ -79,8 +110,15 @@
 	make -f makefile.mk $@ CONFIG_NAME=mygpio_iotoggle_nucleof207zg
 	@echo ""
 	@echo ""
-
-
+	
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_NAME=myuart_printf_nucleof207zg
+	@echo ""
+	@echo ""
+	
+	
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 
