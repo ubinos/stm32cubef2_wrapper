c19a53c54d1af197bcc22931191fa2e66f900f5f
Sungho Park <siminsungho@gmail.com>
2021-02-09 17:27:36 +0900

    refactoring

--- app/ubik_tester/arch/arm/cortexm/nucleof207zg/Inc/main.h
+++ app/ubik_tester/arch/arm/cortexm/nucleof207zg/Inc/main.h
@@ -27,26 +27,6 @@
 
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
-/* User can use this section to tailor TIMx instance used and associated
-   resources */
-/* Definition for TIMx clock resources */
-#define TIMx                           TIM3
-#define TIMx_CLK_ENABLE()              __HAL_RCC_TIM3_CLK_ENABLE()
-
-/* Definition for TIMx Channel Pins */
-#define TIMx_CHANNEL_GPIO_PORT         __HAL_RCC_GPIOB_CLK_ENABLE();
-#define TIMx_GPIO_PORT_CHANNEL1        GPIOB
-#define TIMx_GPIO_PORT_CHANNEL2        GPIOB
-#define TIMx_GPIO_PORT_CHANNEL3        GPIOB
-#define TIMx_GPIO_PORT_CHANNEL4        GPIOB
-#define TIMx_GPIO_PIN_CHANNEL1         GPIO_PIN_4
-#define TIMx_GPIO_PIN_CHANNEL2         GPIO_PIN_5
-#define TIMx_GPIO_PIN_CHANNEL3         GPIO_PIN_0
-#define TIMx_GPIO_PIN_CHANNEL4         GPIO_PIN_1
-#define TIMx_GPIO_AF_CHANNEL1          GPIO_AF2_TIM3
-#define TIMx_GPIO_AF_CHANNEL2          GPIO_AF2_TIM3
-#define TIMx_GPIO_AF_CHANNEL3          GPIO_AF2_TIM3
-#define TIMx_GPIO_AF_CHANNEL4          GPIO_AF2_TIM3
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
 
--- app/ubik_tester/arch/arm/cortexm/stm3221geval/Inc/main.h
+++ app/ubik_tester/arch/arm/cortexm/stm3221geval/Inc/main.h
@@ -27,19 +27,6 @@
 
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
-/* User can use this section to tailor TIMx instance used and associated 
-   resources */
-/* Definition for TIMx clock resources */
-#define TIMx                           TIM3
-#define TIMx_CLK_ENABLE()                __HAL_RCC_TIM3_CLK_ENABLE()
-
-/* Definition for TIMx Pins */
-#define TIMx_CHANNEL_GPIO_PORT()       __HAL_RCC_GPIOC_CLK_ENABLE()
-#define GPIO_PIN_CHANNEL1              GPIO_PIN_6
-#define GPIO_PIN_CHANNEL2              GPIO_PIN_7
-#define GPIO_PIN_CHANNEL3              GPIO_PIN_8
-#define GPIO_PIN_CHANNEL4              GPIO_PIN_9
-  
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
 
