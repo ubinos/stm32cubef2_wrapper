e953626f3311cde28eb9bf9b7720450dd4ffb86a
Sungho Park <siminsungho@gmail.com>
2021-07-30 14:34:22 +0900

    fix infinite waiting in _read function

--- source/Projects/STM322xG_EVAL/Examples/UART/UART_Printf/SW4STM32/syscalls.c
+++ source/Projects/STM322xG_EVAL/Examples/UART/UART_Printf/SW4STM32/syscalls.c
@@ -17,14 +17,14 @@ extern int __io_getchar(void) __attribute__((weak));
 
 __attribute__((weak)) int _read(int file, char *ptr, int len)
 {
-	int DataIdx;
+	/* The I/O library uses an internal buffer */
+	/* It asks for 1024 characters even if only getc() is used. */
+	/* If we use a for(;;) loop on the number of characters requested, */
+	/* the user is forced to enter the exact number requested, even if only one is needed. */
+	/* So here we return only 1 character even if requested length is > 1 */
+	*ptr = __io_getchar();
 
-	for (DataIdx = 0; DataIdx < len; DataIdx++)
-	{
-		*ptr++ = __io_getchar();
-	}
-
-return len;
+	return 1;
 }
 
 __attribute__((weak)) int _write(int file, char *ptr, int len)
